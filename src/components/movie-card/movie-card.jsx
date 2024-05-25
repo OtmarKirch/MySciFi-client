5466d46200effa839406b8f145dda86451f921b9
OtmarKirch <154664034+OtmarKirch@users.noreply.github.com>
2024-05-25 09:21:30 +0200

    Refactored movie-card with react-bootstrap

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,21 +1,33 @@
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = (props) => {
+  const shortDescription =
+    props.movieData.description.substring(0, 100) + "...";
   return (
-    <div
-      onClick={() => {
-        props.onMovieClick(props.movieData);
-      }}
-    >
-      {props.movieData.title}
-    </div>
+    <>
+        <Card
+        onClick={() => {
+          props.onMovieClick(props.movieData);
+        }}
+      >
+        <Card.Img className="movieImage" variant="top" src={props.movieData.imgUrl}/>
+        <Card.Body>
+          <Card.Title>{props.movieData.title}</Card.Title>
+          <Card.Text>{shortDescription}</Card.Text>
+          </Card.Body>
+        <Button variant="link"
+        onClick={() => {
+          props.onMovieClick(props.movieData);
+        }}>Go to movie</Button>
+      </Card>
+    </>
   );
-
 };
 
 MovieCard.propTypes = {
   movieData: PropTypes.shape({
-    title: PropTypes.string
+    title: PropTypes.string,
   }).isRequired,
-  onMovieClick: PropTypes.func.isRequired
-}
+  onMovieClick: PropTypes.func.isRequired,
+};
